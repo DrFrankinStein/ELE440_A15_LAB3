6b6e3b9abb9640ccf45215c2185496bca20d433f
alexpoupouch <tmdudydude@hotmail.com>
2015-11-11 13:49:18 -0500

    deux pseudo-code en code

--- Code/Source/CreerGraphe.cpp
+++ Code/Source/CreerGraphe.cpp
@@ -1,17 +1,27 @@
 #include <cstdlib>
+#include <stdlib.h>
 #include <stdio>
 
 using namespace std;
 
 //CreerGraphe (N, minL, maxL, minP, maxP) : L[0..N-1,0..N-1]
 
-int CreerGraphe(N, MinL, maxL, minP, maxP)
+int CreerGraphe(int N, int minL, int maxL, int minP, int maxP)
 {
 	// Déclarer les variables
 
 	int n;
+	int m;
+	int k;
+	int K;
+	int p;
+	int v;
+	int a;
 
 	//L[0..N-1,o..N-1], numL[0..N-1], tpL[0..N-1]
+	int L[N][N];
+	int numL[N];
+	int tpL[N];
 	//Pour n = 0 à N-1
 	for (n = 0; n < N; n++)
 	{
@@ -19,16 +29,19 @@ int CreerGraphe(N, MinL, maxL, minP, maxP)
 		for (m = 0; m < N; m++)
 		{
 			//Si (n = m)
-			if (n = m)
+			if (n == m)
 			{
 				//L[n,m] = 0
+				L[n][m] = 0;
 			}
 			//Sinon
 			else
 			{
 				//L[n,m] = -1
+				L[n][m] = -1;
 			}
 			//numL[n] = minL + Random() mod  (maxL-minL)
+			numL[n] = minL + rand()%(maxL-minL);
 		}
 	}
 	//Pour k = 1 à minL
@@ -41,23 +54,30 @@ int CreerGraphe(N, MinL, maxL, minP, maxP)
 			if(numL[n] > 0)
 			{
 				//K = 0
+				K = 0;
 				//Pour p = 0 à N-1
-				for()
+				for(p = 0; p < N; p++)
 				{
 					//Si (L[n,p] < 0) et (numL[p] > 0)
-					if()
+					if(L[n,p] < 0 && numL[p] > 0)
 					{
 						//TpL[K] = p
+						tpL[K] = p;
 						//K = K + 1
+						K = K + 1;
 					}
 				}
 				//Si (K > 0)
-				if()
+				if(K > 0)
 				{
 					//v = Random() mod K
+					v = rand()%K;
 					//a = tpL[v]
+					a = tpL[v];
 					//p = minP + Random() mod  (maxP-minP+1)
+					p = minP + rand()%(maxP-minP+1);
 					//L[n,a] = p
+
 					//L[a,n] = p
 					//numL[n] = numL[n] - 1
 					//numL[a] = numL[a] - 1
@@ -65,4 +85,5 @@ int CreerGraphe(N, MinL, maxL, minP, maxP)
 			}
 		}
 	}
+	return 0;
 }
---
+++ Code/Source/rechercheProfondeur.cpp
@@ -0,0 +1,41 @@
+#include <cstdlib>
+#include <stdlib.h>
+#include <stdio>
+
+using namespace std;
+
+//Parcours_Profondeur(A[0..N-1],  Source,  Destination,  Chemin[0..N-1],  p)
+bool ParcoursProfondeur(int A[N], int source, int destination, int Chemin[N], int p)
+//Si A[Source].visité
+if (A[source].visited)
+{
+	//Retourner Faux
+	return false;
+}
+//A[Source].visité = Vrai
+A[source].visited = true;
+//Trouvé = Faux
+found = false;
+k = 0;
+//Tant que (k ≤ A[Source].NVoisins) et (Pas Trouvé)
+while (k <= A[source].voisin && found = false)
+{
+	//v = A[Source].lien[k]
+	v = A[source].link[k];
+	//Si (A[v].nom == Destination)
+	if(A[v].name == destination)
+	{
+		//Trouvé == Vrai
+		found = true;
+		//Chemin[p+1] = A[v].nom
+		Chemin[p+1] = A[v].name;
+	}
+	//Sinon
+	{
+		//Trouvé = Parcours_Profondeur(A, v, Destination, Chemin, p+1)
+		found = ParcoursProfondeur(A, v, destination, chemin)
+Si Pas Trouvé
+k = k + 1
+Si Trouvé
+Chemin[p] =A[Source].nom
+A[Source].visité = Trouvé
