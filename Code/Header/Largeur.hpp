eeca43923e612c83033f3f0e9ed47a897239a578
alexpoupouch <tmdudydude@hotmail.com>
2015-11-13 10:18:38 -0500

    largeur

---
+++ Code/Header/Largeur.hpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Largeur.h
+ * Author: PPoucH
+ *
+ * Created on 12 novembre 2015, 18:28
+ */
+
+#ifndef LARGEUR_H
+#define LARGEUR_H
+
+#include <cstdlib>
+#include <stdlib.h>
+#include <cstdio>
+#include "../Header/Node.hpp"
+
+bool ParcoursLargeur(Node *A, int *sources, int destination, int *Chemin, int p, int n);
+
+#endif /* LARGEUR_H */
+
---
+++ Code/Source/Largeur.cpp
@@ -0,0 +1,81 @@
+#include "../Header/Largeur.hpp"
+
+bool ParcoursLargeur(Node *A, int *sources, int destination, int *Chemin, int p, int n)
+{
+    int i;
+    
+    for(i = 0; i < n; i++)
+    {
+        A[sources[i]].SetVisited(true);
+    }
+    bool found = false;
+    int k = 0;
+    int v;
+    int list[100];
+    int sourceIndex;
+    
+    i = 0;
+    
+    while(i < n && !found)
+    {
+        int j = 0;
+        while (j < A[sources[i]].GetNbNeighbors() && !found)
+        {
+            v = A[sources[i]].GetNeighbor(j);
+            if(A[v].GetID() == destination)
+            {
+                found = true;
+                Chemin[p+1] = A[v].GetID();
+                Chemin[p] = A[sources[i]].GetID();
+                sourceIndex = i;
+            }
+            else
+            {
+                if(!A[v].GetVisited())
+                {
+                    list[k] = A[v].GetID();
+                    k = k + 1;
+                }
+            }
+            if (!found)
+            {
+                j = j + 1;
+            }
+        }
+        if (!found)
+        {
+            i = i + 1;
+        }
+    }
+    if(!found)
+    {
+        found = ParcoursLargeur(A, list, destination, Chemin, p+1, k);
+    }
+    
+    i = 0;
+    int j = 0;
+    if(found)
+    {
+        bool exit = false;
+        while(!exit && i < n)
+        {
+            j = 0;
+            while (!exit && j < A[sources[i]].GetNbNeighbors())
+            {
+                v = A[sources[i]].GetNeighbor(j);
+                if (v == Chemin[p+1])
+                {
+                    Chemin[p] = sources[i];
+                    exit = true;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+            i++;
+        }
+    }
+    A[sources[i-1]].SetVisited(found);
+    return found;
+}
